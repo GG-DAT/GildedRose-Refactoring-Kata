d03097864461df54e18b011a663ab76b9840e00a
pkleinkranenbarg <pkleinkranenbarg@dat.nl>
2024-03-26 13:39:06 +0100

    Refactor existing unit test + make it work

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
+	private static final String ITEM_NAME = "item1";
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void testItemNameDoesNotChangeWhenUpdatingQuality() {
+        final Item[] items = new Item[] { new Item(ITEM_NAME, 0, 0) };
+        final GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(ITEM_NAME, app.items[0].name);
     }
 
 }
